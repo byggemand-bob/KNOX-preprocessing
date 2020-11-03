a8704115c6641cb365bf43319ac989ef387cc8b1
JannikSommer <jannik.sommer@sommerhouse.net>
2020-11-03 14:00:14 +0100

    modified:   Tools/IO_wrapper/manual_Wrapper.py 	modified:   Tools/IO_wrapper/manuals.schema.json 	modified:   Tools/IO_wrapper/output.json 	Tools/IO_wrapper/example.zip 	Tools/IO_wrapper/example/ 	Tools/IO_wrapper/lit/ 	Tools/IO_wrapper/proc.py

--- Tools/IO_wrapper/manual_Wrapper.py
+++ Tools/IO_wrapper/manual_Wrapper.py
@@ -1,25 +1,62 @@
 from knox_source_data_io.io_handler import *
 from knox_source_data_io.models.model import Model
 import os
+import proc
 
 def main():
 
     # Make an object which holds information about the content
     # It should be expanded and iterated upon when we have more work
     class Manual(Model):
-        text: str
+        publisher: str
+        published_at: str
+        title: str
+        sections: []
 
         def __init__(self, values: dict = None, **kwargs):
             values = values if values is not None else kwargs
-            self.text = ""
+            self.text = []
+        
+        def __init__(self, sections):
+            self.sections = sections
+
+    class Section():
+        pages: []
+        paragraphs: []
+
+        def __init__(self, paragraphs):
+            self.pages = [1,2,3]
+            self.paragraphs = paragraphs
+
+
+    class Paragraph():
+        pages: []
+        text: str
+        
+        def __init__(self, string):
+            self.pages = [1,2,3]
+            self.text = string
 
     # Create object and properties
     # Here we should use our tools to crate the objects. 
-    export_able_object = Manual()
-    export_able_object.text = "Here is some text"
+    text = proc.extractLines()
+    file_string_array = []
+    for string in text:
+        file_string_array.append(string)
+
+    file_sections = Section(Paragraph(file_string_array))
+    export_able_object = Model()
+    export_able_object.publisher = "Grundfos A/S"
+    export_able_object.published_at = "Somewhere"
+    export_able_object.title = "Some pump"
+    export_able_object.sections = file_sections
+ 
+
+    
+    # export_able_object.text = proc.extractLines()
 
     # Generate
-    handler = IOHandler(Generator(app="GrundfosManuals_Handler", version=1.0), "/manuals.schema.json")
+    handler = IOHandler(Generator(app="GrundfosManuals_Handler", generated_at="idk", version="1.0"), "/manuals.schema.json")
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, 'output.json')
 
@@ -41,7 +78,7 @@ def create_Output(Manual, schemaPath, outputPath):
     export_able_object = Manual()
 
     # Generate
-    handler = IOHandler(Generator(app="GrundfosManuals_Handler", version=1.0), schemaPath)
+    handler = IOHandler(Generator(app="GrundfosManuals_Handler", generated_at="", version="1.0"), schemaPath)
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, 'output.json') #Rename when we know what the location is
     
