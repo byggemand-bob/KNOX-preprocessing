88b234083f56169e4156332fee70b378b12a0cbb
Jannik Sommer <43030017+JannikSommer@users.noreply.github.com>
2020-11-19 12:16:48 +0100

    Update models.py

--- datastructure/models.py
+++ datastructure/models.py
@@ -1,8 +1,4 @@
 class TextSegment(): 
-    coordinates: Coordinates()
-    header: str
-    value: str
-
     def __init__(self, coordinates, header, value):
         self.coordinates = coordinates
         self.header = header
@@ -10,29 +6,18 @@ class TextSegment():
 
 
 class ImageSegment():
-    coordinates: Coordinates()
-    byte_value: str #bytes
-
     def __init__(self, coordinates, value):
         self.coordinates = coordinates
         self.byte_value = value
 
 
 class TableSegment():
-    coordinates: Coordinates()
-    byte_value: str #bytes
-
     def __init__(self, coordinates, value):
         self.coordinates = coordinates
         self.byte_value = value
 
 
 class Coordinates(): 
-    x1: str
-    x2: str
-    y1: str
-    y2: str
-
     def __init__(self, x1, x2, y1, y2): 
         self.x1 = x1
         self.x2 = x2
