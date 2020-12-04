5cfcbefea2abee84e1e12933e21bf1d667de413d
Magnus Mølgaard Lund <themlund98@gmail.com>
2020-12-04 12:47:32 +0100

    Update pdf2png.py

--- utils/pdf2png.py
+++ utils/pdf2png.py
@@ -1,74 +1,146 @@
+"""
+This module allows conversion of PDF files to PNG files.
+"""
 import os
-import fitz
 import argparse
 import concurrent.futures as cf
+import fitz
+import numpy as np
+from PIL import Image
 
-__zoom = 3
-__verbose = False
 
-def convert(file: str, out_dir: str):
-    mat = fitz.Matrix(__zoom, __zoom)
+ZOOM = 3
+VERBOSE = False
+
+
+def convert_to_file(file: str, out_dir: str):
+    """
+    Converts a PDF file and writes each page as a PNG image in the 'out_dir' directory.
+    """
+
+    mat = fitz.Matrix(ZOOM, ZOOM)
 
     # Open image and get page count
     doc = fitz.open(file)
     number_of_pages = doc.pageCount
 
     # Convert each page to an image
-    for x in range(number_of_pages):
-        page = doc.loadPage(x)
+    for page_number in range(number_of_pages):
+        page = doc.loadPage(page_number)
         pix = page.getPixmap(matrix=mat)
-        outputName = os.path.basename(file).replace(".pdf", "") + "_page" + str(x+1) + ".png"
-        pix.writePNG(os.path.join(out_dir, outputName))
+        output_name = os.path.basename(file).replace(".pdf", "") + "_page" + str(page_number + 1) + ".png"
+        pix.writePNG(os.path.join(out_dir, output_name))
 
-    if __verbose is True:
+    if VERBOSE is True:
         print("  Converted " + file)
 
-def convert_dir(in_dir: str, out_dir: str):
-    
+def convert_dir_to_files(in_dir: str, out_dir: str):
+    """
+    Convert a directory of PDF files and writes each page as a PNG image in the 'out_dir' directory.
+    """
+
+    for file in os.listdir(in_dir):
+        if file.endswith(".pdf"):
+            convert_to_file(os.path.join(in_dir, file), out_dir)
+
+def multi_convert_dir_to_files(in_dir: str, out_dir: str):
+    """
+    Convert a directory of PDF files and writes each page as a PNG image in the 'out_dir' directory.
+    Multi-threaded.
+    """
 
     # Go through every file in the input dir and append to list.
     files = []
     out_dirs = []
-    for file in os.listdir(in_dir,):
+    for file in os.listdir(in_dir):
         if file.endswith(".pdf"):
             files.append(os.path.join(in_dir,file))
             out_dirs.append(out_dir)
 
     with cf.ProcessPoolExecutor() as executor:
-        executor.map(convert, files, out_dirs)
+        executor.map(convert_to_file, files, out_dirs)
 
+def convert_to_matrix(file: str):
+    """
+    Converts a PDF file to image matrices and return a list containing a matrix for each page.
+    """
+
+    mat = fitz.Matrix(ZOOM, ZOOM)
+
+    # Open image and get page count
+    doc = fitz.open(file)
+    number_of_pages = doc.pageCount
+
+    result = []
+
+    # Convert each page to an image
+    for page_number in range(number_of_pages):
+        page = doc.loadPage(page_number)
+
+        #Convert from pdf to PIL format
+        pix = page.getPixmap(matrix=mat)
+        pil_image = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+
+        #C Convert from PIL format to cv2 format
+        cv2_image = np.array(pil_image)
+        result.append(cv2_image)
+
+    return result
+
+#TODO: List could be substituted with dictionary and have filenames as keys
+def convert_dir_to_matrices(in_dir: str):
+    """
+    Convert a directory of PDF files to matrices. Returns a list of lists containing matrices.
+    """
+    result = []
+    for file in os.listdir(in_dir):
+        if file.endswith(".pdf"):
+            result.append(convert_to_matrix(file))
+
+    return result
 
 
 if __name__ == "__main__":
     # Setup command-line arguments
     parser = argparse.ArgumentParser(description="Convert pdf files to png images.")
-    parser.add_argument("in_dir", metavar="IN_DIR", type=str, help="Path the input folder.")
-    parser.add_argument("out_dir", metavar="OUT_DIR", type=str, help="Path to output folder.")
-    parser.add_argument("-z", "--zoom", metavar="N", type=int, default=3, help="Zoom of the PDF conversion.")
-    parser.add_argument("-v", "--verbose", action="store_true", default=False, help="Print more information.")
-    args = parser.parse_args()
+    parser.add_argument("input", metavar = "IN", type = str, help = "Path the input folder.")
+    parser.add_argument("output", metavar = "OUT", type = str, help = "Path to output folder.")
+    parser.add_argument("-z", "--zoom", metavar = "N", type = int, default = 3, help = "Zoom of the PDF conversion.")
+    parser.add_argument("-v", "--verbose", action = "store_true", default = False, help = "Print more information.")
+    parser.add_argument("-m", "--multithreaded", action = "store_true", default = False, help = "Multithread the conversion process. Only for works for folders.")
+    argv = parser.parse_args()
 
-    __zoom = args.zoom
-    __verbose = args.verbose
-
-    # Make sure that input dir exists
-    if not os.path.exists(args.in_dir):
-        raise OSError("Directory not found.")
+    ZOOM = argv.zoom
+    VERBOSE = argv.verbose
 
     # Make sure that output exists. If not, create the dir.
-    if not os.path.exists(args.out_dir):
-        os.mkdir(args.out_dir)
+    if not os.path.isdir(argv.output):
+        print("Output directory must be a correct existing path.")
 
-    # Print the number of files to be converted.
-    num_files = len([f for f in os.listdir(args.in_dir)if os.path.isfile(os.path.join(args.in_dir, f))])
-    if __verbose is True:
-        print("Converting " + str(num_files) + " PDF's")
 
-    # Convert all pdfs.
-    convert_dir(args.in_dir, args.out_dir)
-    
+    if os.path.isfile(argv.input):
+        if argv.input.endswith(".pdf"):
+            print("Converting " + argv.input)
+            convert_to_file(argv.input, argv.output)
+        else:
+            print("Input file must be a PDF file.")
+    elif os.path.isdir(argv.input):
+        # Print the number of files to be converted.
+        num_files = len([f for f in os.listdir(argv.input)if os.path.isfile(os.path.join(argv.input, f))])
+        if VERBOSE is True:
+            print("Converting " + str(num_files) + " PDF's")
+
+        # Convert all pdfs.
+        if argv.multithreaded is True:
+            multi_convert_dir_to_files(argv.input, argv.output)
+        else:
+            convert_dir_to_files(argv.input, argv.output)
+    else:
+        print("Could not find input file/directory.")
+        exit()
+
     # Print number of files created.
-    num_files = len([f for f in os.listdir(args.out_dir)if os.path.isfile(os.path.join(args.out_dir, f))])
-    if __verbose is True:
+    num_files = len([f for f in os.listdir(argv.output)if os.path.isfile(os.path.join(argv.output, f))])
+    if VERBOSE is True:
         print("Created " + str(num_files) + " PNG's")
     exit()
